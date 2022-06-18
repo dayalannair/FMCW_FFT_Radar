3a51c4ab75735bde2e68ba3f6d38ebec40227c26
Dayalan <nairdayalan@gmail.com>
2022-06-18 11:34:49 +0200

    wip

--- FMCW_FFT_Radar.srcs/sim_1/new/FFT_FIFO_TB.v
+++ FMCW_FFT_Radar.srcs/sim_1/new/FFT_FIFO_TB.v
@@ -26,10 +26,10 @@ reg ipClk = 0;
 always #5 ipClk <= ~ipClk;
 reg ipReset = 1;
 
-wire       FFT_output_valid;
+wire[63:0] FFT_output_sample;
+wire FFT_output_valid;
 wire FIFO_write_ready;
 
-UART_PACKET TxPacket;
 wire FIFO_output_valid;
 wire packetiser_ready;
 reg FIFO_read_ready;
@@ -37,36 +37,44 @@ reg FIFO_read_ready;
 reg[4:0] ipButtons;
 
 wire[63:0] FIFO_output_data;
-reg[8:0] FIFO_Length;
+wire[8:0] FIFO_Length;
 FFT_FIFO FFT(
     .ipClk (ipClk),
-    .ipReset (~ipnReset),
+    .ipReset (ipReset),
     // Run FFT on button push
     .ipRunFFT (ipButtons[0]),
     .m_axis_data_tdata (FFT_output_sample),
     .m_axis_data_tready (FIFO_write_ready),
     .m_axis_data_tvalid (FFT_output_valid)
 );
+
 FIFO_custom FIFO(
     .ipClk (ipClk),
-    .ipReset (~ipnReset),
+    .ipReset (ipReset),
 
     .write_valid (FFT_output_valid),
     .write_ready (FIFO_write_ready),
     .write_data (FFT_output_sample),
     
-    .read_ready (FIFO_read_ready&packetiser_ready),
+    .read_ready (FIFO_read_ready),
     .read_valid (FIFO_output_valid),
     .read_data (FIFO_output_data),
 
     .FIFO_Length (FIFO_Length) // FIFO length is position of write pointer
     );
 
+reg[31:0] Re_out;
+reg[31:0] Im_out;
+
+reg[31:0] Re_in;
+reg[31:0] Im_in;
+
 initial begin
   @(posedge ipClk);
   @(posedge ipClk);
   @(posedge ipClk);
   ipReset <= 0;
+  FIFO_read_ready <= 1;
   //Initiate FFT
   #100
   ipButtons <= 5'b1;
@@ -74,6 +82,19 @@ initial begin
   ipButtons <= 5'b0;
 end
 
+always@ (posedge ipClk) begin
+    if (ipReset) begin
+        ipButtons <= 0;
+    end
+    
+    else begin
+        Re_in <= $signed(FFT_output_sample[31:0]);
+        Im_in <= $signed(FFT_output_sample[63:32]);
+        
+        Re_out <= $signed(FIFO_output_data[31:0]);
+        Im_out <= $signed(FIFO_output_data[63:32]);
+    end
+end
 
 
 endmodule
