b4d2053e34002184dea2fbd3b1ce537fa5fecfc9
dayalannair <nairdayalan@gmail.com>
2022-06-24 10:48:07 +0200

    working

--- combined_program_dir/FFT_multi_sweep.m
+++ combined_program_dir/FFT_multi_sweep.m
@@ -1,10 +1,4 @@
 format longg
-% FPGA_Re_FFT_tab = readtable('..\FMCW_FFT_Radar.sim\sim_1\behav\xsim\FFT_out_Re.txt','Delimiter' ,'\n');
-% FPGA_Im_FFT_tab = readtable('..\FMCW_FFT_Radar.sim\sim_1\behav\xsim\FFT_out_Im.txt','Delimiter' ,'\n');
-
-% OUTPUT FROM NEXYS A7
-FPGA_Re_FFT_tab = readtable('FFT_Re.txt','Delimiter' ,'\n');
-FPGA_Im_FFT_tab = readtable('FFT_Im.txt','Delimiter' ,'\n');
 
 raw_Re_tab = readtable('IQ_data/I_trolley_test.txt','Delimiter' ,' ');
 raw_Im_tab = readtable('IQ_data/Q_trolley_test.txt','Delimiter' ,' ');
@@ -17,15 +11,18 @@ desired_sweep = 50; % sweep passed to FPGA
 % only up sweep is taken
 i_dat(:,1:200) = table2array(raw_Re_tab(:,1:200));
 q_dat(:,1:200) = table2array(raw_Im_tab(:,1:200));
-multi_sweep_to_hex(i_dat, q_dat, 5);
+multi_sweep_to_hex(i_dat, q_dat, 344);
 %%
+% OUTPUT FROM NEXYS A7
+FPGA_Re_FFT_tab = readtable('FFT_Re.txt','Delimiter' ,' ');
+FPGA_Im_FFT_tab = readtable('FFT_Im.txt','Delimiter' ,' '); 
 Re = table2array(FPGA_Re_FFT_tab);
 Im = table2array(FPGA_Im_FFT_tab);
 % Correction of current error:
 % 1. reverse right shift
 % 2. remove incorrect sample
-Re(1) = Re(end);
-Im(1) = Im(end);
+Re(:,1) = Re(:,end);
+Im(:,1) = Im(:,end);
 Re = circshift(Re,-1);
 Im = circshift(Im,-1);
 
@@ -41,8 +38,17 @@ close all
 
 fig = figure;
 fig.WindowState = 'maximized';
-
-plot(f/1000,fftshift(abs(MATLAB_FFT)))
+tiledlayout(2,1)
+nexttile
+plot(f/1000, 20*log10(fftshift(abs(FPGA_FFT))))
+title("FPGA FFT Magnitude (fftshifted)")
+xlabel("Frequency (kHz)")
+ylabel("Magnitude (dB)")
+%axis([-100 100 0 10e5])
+nexttile
+plot(f/1000,20*log10(fftshift(abs(MATLAB_FFT))))
 title("MATLAB FFT Magnitude (fftshifted)")
 xlabel("Frequency (kHz)")
-axis([-100 100 0 10e5])
+ylabel("Magnitude (dB)")
+%axis([-100 100 0 10e5])
+
