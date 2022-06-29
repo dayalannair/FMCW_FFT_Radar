de1341caa3dcb2a74246c040a927687f4152b154
dayalannair <nairdayalan@gmail.com>
2022-06-29 06:30:50 +0200

    wip

--- FINAL_PROGRAM_FILES/single_sweep/FFT_results.m
+++ FINAL_PROGRAM_FILES/single_sweep/FFT_results.m
@@ -3,39 +3,61 @@ format longg
 % FPGA_Im_FFT_tab = readtable('..\FMCW_FFT_Radar.sim\sim_1\behav\xsim\FFT_out_Im.txt','Delimiter' ,'\n');
 
 % OUTPUT FROM NEXYS A7
-FPGA_Re_FFT_tab = readtable('FFT_Re.txt','Delimiter' ,'\n');
-FPGA_Im_FFT_tab = readtable('FFT_Im.txt','Delimiter' ,'\n');
+FPGA_Re_FFT_tab = readtable('../FFT_Re.txt','Delimiter' ,' ');
+FPGA_Im_FFT_tab = readtable('../FFT_Im.txt','Delimiter' ,' ');
 
-raw_Re_tab = readtable('IQ_data/I_trolley_test.txt','Delimiter' ,' ');
-raw_Im_tab = readtable('IQ_data/Q_trolley_test.txt','Delimiter' ,' ');
+raw_Re_tab = readtable('../IQ_data/I_trolley_test.txt','Delimiter' ,' ');
+raw_Im_tab = readtable('../IQ_data/Q_trolley_test.txt','Delimiter' ,' ');
+%%
 
 % PADDING ONLY NEEDED FOR MATLAB
 % FPGA has pre-padded buffers
 i_dat = zeros(1, 256);
 q_dat = zeros(1, 256);
 
-desired_sweep = 50; % sweep passed to FPGA
+% sweep to be analysed
+desired_sweep = 50; 
 
 % only up sweep is taken
 i_dat(1:200) = table2array(raw_Re_tab(desired_sweep,1:200));
 q_dat(1:200) = table2array(raw_Im_tab(desired_sweep,1:200));
+
+% format data for FFT. must re-run this program
+% on received data
+% if desired sweep is changed
 sweep_to_hex(i_dat, q_dat);
-%%
-i_dat_signed = int16(i_dat);
-Re = table2array(FPGA_Re_FFT_tab);
-Im = table2array(FPGA_Im_FFT_tab);
-% Correction of current error:
-% 1. reverse right shift
-% 2. remove incorrect sample
-Re(1) = Re(end);
-Im(1) = Im(end);
-Re = circshift(Re,-1);
-Im = circshift(Im,-1);
+
+% FPGA FFT. Uses desired sweep as FPGA FFT data is from
+% multiple sweeps (same data set)
+Re = table2array(FPGA_Re_FFT_tab(desired_sweep,:));
+Im = table2array(FPGA_Im_FFT_tab(desired_sweep,:));
 
 FPGA_FFT = Re + 1i*Im;
-% Matlab FFT for comparison
+%%
+% Matlab FFT
+% get runtime
+format compact
+format short eng
+
+Time = [];
+
+for m = 1000
+  start = tic();
+  iq = i_dat + 1i*q_dat;
+  MATLAB_FFT = fft(iq, [], 2);
+  Time = [Time (toc(start) / m)];
+end
+%%
+start = tic();
 iq = i_dat + 1i*q_dat;
 MATLAB_FFT = fft(iq, [], 2);
+toc(start)
+Time = (toc(start) / 1);
+
+
+
+% Time = [Time (toc(start) / m)];
+
 %% Plots
 fs = 200e3;
 N = 256;
